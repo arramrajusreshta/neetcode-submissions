6a44eb6b50d375b8dcb2c73ee20032c7bd450d80
arramrajusreshta <arramraju.sreshta@gmail.com>
2026-09-07 11:11:24 +0530

    Add: final-array-state-after-k-multiplication-operations-i - submission-0

---
+++ Data Structures & Algorithms/final-array-state-after-k-multiplication-operations-i/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        for _ in range(k):
+            min_val=min(nums)
+            i=nums.index(min_val)
+            nums[i]=min_val*multiplier
+        return nums
+
